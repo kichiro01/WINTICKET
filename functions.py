ebf72fca590885424ff607c5f2163e848eba4b6b
ICHIRO KAWATA <kichiro01@gmail.com>
2021-09-19 20:24:22 +0900

    first commit

---
+++ functions.py
@@ -0,0 +1,128 @@
+import config
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+flag = True
+
+def is_right_page(driver, page):
+    if page == config.LOGIN_PAGE_URL:
+        #5秒間の間にユーザーID入力フォームが表示されなければログイン画面に遷移していないと判断する
+        try:
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, '_userId')))
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            return True
+    else:
+        print('target_URLが不正です。')
+        return False
+    
+def loop_access(driver, target_url):
+    while(True):
+        print('Accessing to target URL...')
+        driver.get(target_url)
+        print('Current URL：{0}'.format(driver.current_url))
+        #ターゲットURLじゃなかったらリロードにした方がいいかも
+
+        if is_right_page(driver, target_url):
+            print('ACCESS SUCCEED')
+            break
+        else:
+            print('ロードに時間がかかっているか、アクセス集中ページにリダイレクトされました。')
+    return
+
+#ログイン処理
+def login(driver):
+    #ログインページにたどり着くまでループアクセス
+    print('Accessing to login page...')
+    loop_access(driver, config.LOGIN_PAGE_URL)
+
+    #全てのエレメントが表示されるまで待つ（30秒でタイムアウト）
+    WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located)
+    #ログイン情報入力
+    driver.find_element_by_id('_userId').send_keys(config.LOGIN_USER_ID)
+    driver.find_element_by_id('_password').send_keys(config.LOGIN_PW)
+
+    #ログインボタンがクリックできる状態まで明示的に待機。30秒でタイムアウト
+    try:
+        element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.NAME, 'confirm')))
+    except:
+        print('ログインボタン活性化タイムアウト')
+    else:
+        #ログインボタン押下
+        driver.execute_script('arguments[0].click();', element)
+    return
+
+def move_to_top_page(driver):
+    element = None
+    #トップページにあるはずのチケットボタンが確認できない場合ログインからやり直し続ける
+    while(True):
+        try:
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//img[@alt="チケット"]')))
+        except:
+            #再度ログイン
+            print('再ログインします。')
+            login(driver)
+            continue
+        else:
+            break
+    
+    #該当ページに到達するまで10個ずつタブを開いてはチェックして消すを繰り返す
+    i = 0
+    global flag
+    while(flag):
+        i += 1
+        print('10タブ展開{0}回目....'.format(i))
+        driver.switch_to.window(driver.window_handles[0])
+        open_ticket_page(driver)
+    
+    #チケットのページへのリンクをクリック
+    # ActionChains(driver).move_to_element(element).perform()
+    # driver.find_element_by_link_text('パークチケット').click()
+    return
+
+def open_ticket_page(driver):
+    #新規タブを３つ開いてでチケットページにアクセス
+    for i in range(3):
+        driver.execute_script("window.open(arguments[0], '_blank')", config.TICKET_PAGE_URL)
+        #一応人が手で連打するくらいの感覚は開けてアクセス
+        time.sleep(0.2)
+
+    #タブが全て開かれるのを待つ
+    WebDriverWait(driver, 30).until(EC.number_of_windows_to_be(4))
+    #ちゃんとチケットページが表示されているタブまで移動し続ける
+    for i in range(3):
+        print('i')
+        print(i)
+        #操作可能なタブを取得し、２つ目のタブに移動
+        tabs = driver.window_handles
+        driver.switch_to.window(tabs[1])
+        try:
+            #画面前要素表示を待機後次へボタンがあるかどうか確認（1秒でタイムアウト）
+            WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located)
+            element = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CLASS_NAME, 'slick-next')))
+        except:
+            #次へボタンがなければタブを閉じて次のタブ（閉じているから再度２つ目のタブ）へ
+            print('このタブには次へボタンがない。')
+            driver.close()
+        else:
+            #タプ展開＆チェックの無限ループを終了しチケット選択処理へ
+            global flag
+            flag = False
+            select_conditions(driver)
+            break
+            
+    return
+
+def select_conditions(driver):
+    #カレンダーを捲るボタン、日程選択ボタン、自宅でプリントボタンはそれぞれ離れた場所にあり、クリックのタイミングで次のボタンがウィンドウ外にあるためjavascriptからクリックを実行。
+    next_button = driver.find_element_by_class_name('slick-next')
+    day_button = driver.find_element_by_xpath('//a[@data-daynumbercount="60"]')
+    search_button = driver.find_element_by_id("searchEticket")
+    driver.execute_script('arguments[0].click(); arguments[1].click(); arguments[2].click()', next_button, day_button, search_button)
+    return
---
+++ main.py
@@ -0,0 +1,26 @@
+
+from selenium import webdriver
+import functions
+import time
+
+
+# WebDriver のオプションを設定する
+options = webdriver.ChromeOptions()
+#画像読み込みを省略して軽量化
+options.add_argument('--blink-settings=imagesEnabled=false')
+#↓動作確認のためコメントアウト
+#options.add_argument('--headless') 
+
+print('connecting to remote browser...')
+#chromedriver起動
+driver = webdriver.Chrome(options=options)
+#ページロード中などfind_elementで要素が見つからない際の暗黙的な待機を最大30秒に設定　▶各処理に対する明示的待機にて上書き不能のため、全体に適用される暗黙待機は実装しない
+#driver.implicitly_wait(30)
+
+functions.login(driver)
+functions.move_to_top_page(driver)
+
+# ブラウザを終了する(最終画面からの操作猶予30分)
+time.sleep(1800)
+driver.quit()
+
